5e6138631265d8486c3360d0faa4f0cd85a3f708
Modlich Adam <adam.modlich13@gmail.com>
2024-06-28 20:55:47 +0200

    PI-3: Changed order of files to better image project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './main-page/navbar/navbar.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { CalendarComponent } from './main-page/calendar/calendar.component';
-import { TodaySectionComponent } from './main-page/today-section/today-section.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import {CalendarModule} from "primeng/calendar";
+import { CalendarModule } from "primeng/calendar";
+import { FilterSectionComponent } from "./main-page/filter-section/filter-section.component";
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import {CalendarModule} from "primeng/calendar";
     NavbarComponent,
     MainPageComponent,
     CalendarComponent,
-    TodaySectionComponent
+    FilterSectionComponent,
   ],
   imports: [
     BrowserModule,
