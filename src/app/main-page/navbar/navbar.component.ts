9905b91965246e9f61fdc4eb95c2869e2c288879
adammodlich <adam.modlich13@gmail.com>
2024-07-06 21:41:35 +0200

    PI-4: Changing weeks added

--- src/app/main-page/navbar/navbar.component.ts
+++ src/app/main-page/navbar/navbar.component.ts
@@ -1,25 +1,86 @@
-import {Component, computed, OnInit} from '@angular/core';
+import {AfterViewInit, Component, computed, ViewChild} from '@angular/core';
 import {CalendarService} from "../../services/calendar.service";
 import {ViewMode} from "../../utils/viewModeEnum.model";
+import {Calendar} from "primeng/calendar";
+import moment from "moment";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  public ViewModeEnum = ViewMode;
+export class NavbarComponent implements AfterViewInit {
+  @ViewChild(Calendar) calendar: Calendar | undefined;
+
   public viewMode = computed(() => this.calendarService.viewModeChanges());
+  public ViewModeEnum = ViewMode;
+  public selectedDate: Date = new Date();
 
   constructor(private calendarService: CalendarService) {}
 
-  public getDateFormat(): string {
-    return this.viewMode() === ViewMode.DAY
-      ? 'D/M/yy'
-      : 'MM yy';
+  public ngAfterViewInit(): void {
+    this.setWeeklyDateFormat();
   }
 
   public changeCalendarViewMode(viewMode: ViewMode): void {
-    this.calendarService.setViewMode(viewMode)
+    this.calendarService.setViewMode(viewMode);
+    if (this.calendar) {
+      switch (this.viewMode()) {
+        case ViewMode.DAY:
+          this.calendar.dateFormat = 'dd/mm/yy';
+          break;
+        case ViewMode.WEEK:
+          this.setWeeklyDateFormat();
+          break;
+        case ViewMode.MONTH:
+          this.calendar.dateFormat = 'MM yy';
+      }
+      this.calendar?.updateInputfield();
+    }
+  }
+
+  public changeToNextPeriod(): void {
+    switch (this.viewMode()) {
+      case ViewMode.DAY:
+        this.selectedDate.setDate(this.selectedDate.getDate() + 1);
+        break;
+      case ViewMode.WEEK:
+        this.selectedDate.setDate(this.selectedDate.getDate() + 7);
+        this.setWeeklyDateFormat();
+        break;
+      case ViewMode.MONTH:
+        this.selectedDate.setMonth(this.selectedDate.getMonth() + 1);
+        break;
+    }
+    this.calendar?.updateInputfield();
+  }
+
+  public changeToPreviousPeriod(): void {
+    switch (this.viewMode()) {
+      case ViewMode.DAY:
+        this.selectedDate.setDate(this.selectedDate.getDate() - 1);
+        break;
+      case ViewMode.WEEK:
+        this.selectedDate.setDate(this.selectedDate.getDate() - 7);
+        this.setWeeklyDateFormat();
+        break;
+      case ViewMode.MONTH:
+        this.selectedDate.setMonth(this.selectedDate.getMonth() - 1);
+        break;
+    }
+    this.calendar?.updateInputfield();
+  }
+
+  public getWeekendDates(): string {
+    const beginWeekendDate = moment(this.selectedDate).clone().weekday(1).format('DD/MM');
+    const endWeekendDate = moment(this.selectedDate).clone().weekday(7).format('DD/MM')
+    return  beginWeekendDate + ' - ' + endWeekendDate + ' ' + this.selectedDate.getFullYear();
+  }
+
+  private setWeeklyDateFormat(): void {
+    if (this.calendar) {
+      this.calendar.dateFormat = this.getWeekendDates();
+      this.calendar?.updateInputfield();
+    }
   }
 }
