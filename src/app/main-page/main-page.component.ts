b62558ef7b09fc0f82f5daf9523b693a991dd75c
Modlich Adam <adam.modlich13@gmail.com>
2024-11-10 19:45:34 +0100

    PI-5: Created scheduler grid

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, computed} from '@angular/core';
+import {CalendarService} from "../services/calendar.service";
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  public viewMode = computed(() => this.calendarService.viewModeChanges());
   public date: Date = new Date();
+
+  constructor(private calendarService: CalendarService) {}
 }
